2ab6763f83214553ac5acbced317e40aec9e9daf
BobbyValet304 <jalenwilliams90@yahoo.com>
2024-06-13 09:49:48 -0400

    fixes bugs, changes salary from string to number

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,16 +14,17 @@ const collectEmployees = function () {
     //We need to create an employee
     //We need to take this employee and add to data
     const employee = {
-      first: firstName,
-      last: lastName,
-      pay: salary,
-      //salary is currently a string and needs to be an integer
+      firstName: firstName,
+      lastName: lastName,
+      salary: parseInt(salary),
+      //salary is was a string and is now an integer
     };
     data.push(employee);
 
     accept = confirm("Would you like to add another employee?");
   }
   console.log(data);
+  return data;
 };
 
 // Display the average salary
